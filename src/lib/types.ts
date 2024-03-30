96a4e4f6bbb43367d702fdde2a3686f439ecb285
slicycode <deparisjules180@gmail.com>
2024-03-30 18:38:24 +0100

    feat: add settings page

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,15 @@
+import { z } from 'zod'
+
+export const EditUserProfileSchema = z.object({
+  email: z.string().email('Required'),
+  name: z.string().min(1, 'Required'),
+})
+
+export const WorkflowFormSchema = z.object({
+  name: z.string().min(1, 'Required'),
+  description: z.string().min(1, 'Required'),
+})
+
 export type ConnectionTypes = 'Google Drive' | 'Notion' | 'Slack' | 'Discord'
 
 export type Connection = {
